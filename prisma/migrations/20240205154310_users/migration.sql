1ae442581503a746f371c779fd5a578b88e57b06
Abdelrahman Elassal <122584703+AbdelrahmanElassal@users.noreply.github.com>
2024-02-05 17:44:00 +0200

    model

---
+++ prisma/migrations/20240205154310_users/migration.sql
@@ -0,0 +1,25 @@
+/*
+  Warnings:
+
+  - You are about to drop the `post` table. If the table is not empty, all the data it contains will be lost.
+  - You are about to drop the `user` table. If the table is not empty, all the data it contains will be lost.
+
+*/
+-- DropForeignKey
+ALTER TABLE `post` DROP FOREIGN KEY `Post_authorId_fkey`;
+
+-- DropTable
+DROP TABLE `post`;
+
+-- DropTable
+DROP TABLE `user`;
+
+-- CreateTable
+CREATE TABLE `users` (
+    `email` VARCHAR(191) NOT NULL,
+    `username` VARCHAR(191) NOT NULL,
+    `password` VARCHAR(191) NOT NULL,
+
+    UNIQUE INDEX `users_username_key`(`username`),
+    PRIMARY KEY (`email`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
